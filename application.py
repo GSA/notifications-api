226815b7d822713e17551a1f6615838a7fea0b1e
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2020-06-16 13:59:15 +0100

    more wip

--- application.py
+++ application.py
@@ -2,8 +2,11 @@
 from __future__ import print_function
 
 from flask import Flask
+import psycogreen.eventlet
 
 from app import create_app
+
+psycogreen.eventlet.patch_psycopg()
 
 application = Flask('app')
 
