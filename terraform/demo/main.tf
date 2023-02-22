eec618dff840a9ef40791705b6b13684249a5cfe
Ryan Ahearn <ryan.ahearn@gsa.gov>
2023-02-22 10:10:00 -0500

    Enable sns service in demo

--- terraform/demo/main.tf
+++ terraform/demo/main.tf
@@ -68,17 +68,13 @@ module "ses_email" {
   email_receipt_error = "notify-support@gsa.gov"
 }
 
-#########################################################################
-# Wait for SNS is out of sandbox and spending limit is increased
-# before activating this module
-#########################################################################
-# module "sns_sms" {
-#   source = "../shared/sns"
+module "sns_sms" {
+  source = "../shared/sns"
 
-#   cf_org_name         = local.cf_org_name
-#   cf_space_name       = local.cf_space_name
-#   name                = "${local.app_name}-sns-${local.env}"
-#   recursive_delete    = local.recursive_delete
-#   aws_region          = "us-east-1"
-#   monthly_spend_limit = 25
-# }
+  cf_org_name         = local.cf_org_name
+  cf_space_name       = local.cf_space_name
+  name                = "${local.app_name}-sns-${local.env}"
+  recursive_delete    = local.recursive_delete
+  aws_region          = "us-east-1"
+  monthly_spend_limit = 25
+}
