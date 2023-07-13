ca3721d92f7618e3f7e3de9f3a4c875d8dbc301b
stvnrlly <steven.reilly@gsa.gov>
2023-07-13 09:40:14 -0400

    rename organisation_types table

---
+++ migrations/versions/0397_rename_organisation_2.py
@@ -0,0 +1,21 @@
+"""
+
+Revision ID: 0397_rename_organisation_2
+Revises: 0396_rename_organisation
+Create Date: 2023-07-13 09:33:52.455290
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '0397_rename_organisation_2'
+down_revision = '0396_rename_organisation'
+
+
+def upgrade():
+    op.execute('ALTER TABLE organisation_types RENAME to organization_types')
+
+
+def downgrade():
+    op.execute('ALTER TABLE organization_types RENAME to organisation_types')
