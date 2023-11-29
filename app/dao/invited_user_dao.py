c04df94b2294a034c562cb8e172269760519a1b6
Cliff Hill <Clifford.hill@gsa.gov>
2023-11-29 16:08:02 -0500

    Adjusting some DAO stuff to send expired invites now.
    Signed-off-by: Cliff Hill <Clifford.hill@gsa.gov>

--- app/dao/invited_user_dao.py
+++ app/dao/invited_user_dao.py
@@ -13,26 +13,19 @@ def get_invited_user_by_service_and_id(service_id, invited_user_id):
     return InvitedUser.query.filter(
         InvitedUser.service_id == service_id,
         InvitedUser.id == invited_user_id,
-        InvitedUser.status != INVITE_EXPIRED,
     ).one()
 
 
 def get_invited_user_by_id(invited_user_id):
-    return InvitedUser.query.filter(
-        InvitedUser.id == invited_user_id, InvitedUser.status != INVITE_EXPIRED
-    ).one()
+    return InvitedUser.query.filter(InvitedUser.id == invited_user_id).one()
 
 
 def get_expired_invited_users_for_service(service_id):
-    return InvitedUser.query.filter(
-        InvitedUser.service_id == service_id, InvitedUser.status == INVITE_EXPIRED
-    ).all()
+    return InvitedUser.query.filter(InvitedUser.service_id == service_id).all()
 
 
 def get_invited_users_for_service(service_id):
-    return InvitedUser.query.filter(
-        InvitedUser.service_id == service_id, InvitedUser.status != INVITE_EXPIRED
-    ).all()
+    return InvitedUser.query.filter(InvitedUser.service_id == service_id).all()
 
 
 def expire_invitations_created_more_than_two_days_ago():
