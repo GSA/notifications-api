b63130c1bad4d64fe70da9ee392abf1a0895ef74
Carlo Costino <carlo.costino@gsa.gov>
2024-01-30 10:35:27 -0500

    Remove profile attribute from environment providers
    Signed-off-by: Carlo Costino <carlo.costino@gsa.gov>

--- terraform/sandbox/providers.tf
+++ terraform/sandbox/providers.tf
@@ -12,7 +12,7 @@ terraform {
     key     = "api.tfstate.sandbox"
     encrypt = "true"
     region  = "us-gov-west-1"
-    profile = "notify-terraform-backend"
+    # profile = "notify-terraform-backend"
   }
 }
 
