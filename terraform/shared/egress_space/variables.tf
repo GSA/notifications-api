f0e66886d21dc2c41f8c53db4477834ce7432ae1
Carlo Costino <carlo.costino@gsa.gov>
2024-03-12 14:26:28 -0400

    Adding missing variable for egress_space
    Signed-off-by: Carlo Costino <carlo.costino@gsa.gov>

--- terraform/shared/egress_space/variables.tf
+++ terraform/shared/egress_space/variables.tf
@@ -3,3 +3,9 @@ variable "cf_restricted_space_name" {}
 variable "deployers" {
   type = set(string)
 }
+
+variable "delete_recursive_allowed" {
+  type        = bool
+  default     = true
+  description = "Flag for allowing resources to be recursively deleted - not recommended in production environments"
+}
