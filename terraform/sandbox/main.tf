2941f2ed38187af0220fbf3947ac54c5daed7984
John Skiles Skinner <john.skinner@gsa.gov>
2024-04-16 16:43:24 -0700

    Remove recursive_delete, add prevent_destroy

--- terraform/sandbox/main.tf
+++ terraform/sandbox/main.tf
@@ -1,9 +1,8 @@
 locals {
-  cf_org_name      = "gsa-tts-benefits-studio"
-  cf_space_name    = "notify-sandbox"
-  env              = "sandbox"
-  app_name         = "notify-api"
-  recursive_delete = true
+  cf_org_name   = "gsa-tts-benefits-studio"
+  cf_space_name = "notify-sandbox"
+  env           = "sandbox"
+  app_name      = "notify-api"
 }
 
 data "cloudfoundry_org" "org" {
@@ -15,35 +14,35 @@ resource "cloudfoundry_space" "notify-sandbox" {
   delete_recursive_allowed = true
   name                     = local.cf_space_name
   org                      = data.cloudfoundry_org.org.id
+  lifecycle {
+    prevent_destroy = false # sandbox is ephemeral; delete and destroy OK
+  }
 }
 
 module "database" {
   source = "github.com/18f/terraform-cloudgov//database?ref=v0.7.1"
 
-  cf_org_name      = local.cf_org_name
-  cf_space_name    = local.cf_space_name
-  name             = "${local.app_name}-rds-${local.env}"
-  recursive_delete = local.recursive_delete
-  rds_plan_name    = "micro-psql"
+  cf_org_name   = local.cf_org_name
+  cf_space_name = local.cf_space_name
+  name          = "${local.app_name}-rds-${local.env}"
+  rds_plan_name = "micro-psql"
 }
 
 module "redis" {
   source = "github.com/18f/terraform-cloudgov//redis?ref=v0.7.1"
 
-  cf_org_name      = local.cf_org_name
-  cf_space_name    = local.cf_space_name
-  name             = "${local.app_name}-redis-${local.env}"
-  recursive_delete = local.recursive_delete
-  redis_plan_name  = "redis-dev"
+  cf_org_name     = local.cf_org_name
+  cf_space_name   = local.cf_space_name
+  name            = "${local.app_name}-redis-${local.env}"
+  redis_plan_name = "redis-dev"
 }
 
 module "csv_upload_bucket" {
   source = "github.com/18f/terraform-cloudgov//s3?ref=v0.7.1"
 
-  cf_org_name      = local.cf_org_name
-  cf_space_name    = local.cf_space_name
-  recursive_delete = local.recursive_delete
-  name             = "${local.app_name}-csv-upload-bucket-${local.env}"
+  cf_org_name   = local.cf_org_name
+  cf_space_name = local.cf_space_name
+  name          = "${local.app_name}-csv-upload-bucket-${local.env}"
 }
 
 module "egress-space" {
@@ -64,7 +63,6 @@ module "ses_email" {
   cf_org_name         = local.cf_org_name
   cf_space_name       = local.cf_space_name
   name                = "${local.app_name}-ses-${local.env}"
-  recursive_delete    = local.recursive_delete
   aws_region          = "us-west-2"
   email_receipt_error = "notify-support@gsa.gov"
 }
@@ -75,7 +73,6 @@ module "sns_sms" {
   cf_org_name         = local.cf_org_name
   cf_space_name       = local.cf_space_name
   name                = "${local.app_name}-sns-${local.env}"
-  recursive_delete    = local.recursive_delete
   aws_region          = "us-east-2"
   monthly_spend_limit = 1
 }
