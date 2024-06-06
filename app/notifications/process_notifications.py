6081ff48738214020e362fcdfae139c9d5443afb
Kenneth Kehl <@kkehl@flexion.us>
2024-06-06 13:18:00 -0700

    merge from main

--- app/notifications/process_notifications.py
+++ app/notifications/process_notifications.py
@@ -13,7 +13,6 @@ from app.enums import KeyType, NotificationStatus, NotificationType
 from app.errors import BadRequestError
 from app.models import Notification
 from app.utils import hilite, scrub, utc_now
-from app.errors import BadRequestError
 from notifications_utils.recipients import (
     format_email_address,
     get_international_phone_info,
@@ -111,7 +110,11 @@ def persist_notification(
         formatted_recipient = validate_and_format_phone_number(
             recipient, international=True
         )
-        current_app.logger.info(hilite(scrub(f"Persisting notification with recipient {formatted_recipient}")))
+        current_app.logger.info(
+            hilite(
+                scrub(f"Persisting notification with recipient {formatted_recipient}")
+            )
+        )
         recipient_info = get_international_phone_info(formatted_recipient)
         notification.normalised_to = formatted_recipient
         notification.international = recipient_info.international
