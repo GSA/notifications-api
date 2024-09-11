7fe00e6d1420c11a01c856520428774401349401
Kenneth Kehl <@kkehl@flexion.us>
2024-09-11 09:57:32 -0700

    code review feedback and merge from main

--- tests/app/test_cronitor.py
+++ tests/app/test_cronitor.py
@@ -81,7 +81,7 @@ def test_cronitor_does_nothing_if_name_not_recognised(notify_api, rmock, mocker)
     ):
         assert successful_task() == 1
 
-    mock_logger.exception.assert_called_with(
+    mock_logger.error.assert_called_with(
         "Cronitor enabled but task_name hello not found in environment"
     )
 
