5277f7066035b6f7e0c444f94d3083f8b2b820b1
Kenneth Kehl <@kkehl@flexion.us>
2024-10-03 09:59:41 -0700

    add a test in service rest

--- tests/app/service/test_rest.py
+++ tests/app/service/test_rest.py
@@ -1974,9 +1974,8 @@ def test_get_monthly_notification_stats_by_user(
     auth_header = create_admin_authorization_header()
 
     response = client.get(
-        path=(f"/service/{sample_service.id}/notifications/{sample_user.id}/monthly"),
+        path=(f"/service/{sample_service.id}/notifications/{sample_user.id}/monthly?year=2024"),
         headers=[auth_header],
-        year="2024",
     )
 
     resp = json.loads(response.get_data(as_text=True))
