4f3b99b8baec4f49cdb3fdc549fcaf5b601c0174
Kenneth Kehl <@kkehl@flexion.us>
2024-11-18 09:36:41 -0800

    more

--- tests/app/dao/test_service_email_reply_to_dao.py
+++ tests/app/dao/test_service_email_reply_to_dao.py
@@ -208,7 +208,7 @@ def test_update_reply_to_email_address_set_updated_to_default(sample_service):
         is_default=True,
     )
 
-    results = db.session.execute(select(ServiceEmailReplyTo)).scalars().all()
+    results = db.session.execute(select(ServiceEmailReplyTo)).all()
     assert len(results) == 2
     for x in results:
         if x.email_address == "change_address@email.com":
