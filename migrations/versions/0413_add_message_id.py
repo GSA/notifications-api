69ecb2f096ee0fbca35fa593c6cdd50f953c3e56
Kenneth Kehl <@kkehl@flexion.us>
2024-12-10 21:00:31 -0800

    add message_id column to notifications

--- migrations/versions/0413_add_message_id.py
+++ migrations/versions/0413_add_message_id.py
@@ -15,7 +15,14 @@ down_revision = "0412_remove_priority"
 
 def upgrade():
     op.add_column("notifications", sa.Column("message_id", sa.Text))
+    op.create_index(
+        "ix_notifications_message_id",
+        "notifications",
+        ["message_id"],
+        unique=False,
+    )
 
 
 def downgrade():
+    op.drop_index("ix_notifications_message_id", table_name="notifications")
     op.drop_column("notifications", "message_id")
