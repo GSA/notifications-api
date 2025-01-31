048126caca31518bd4c6877eea44c046ea99c008
Kenneth Kehl <@kkehl@flexion.us>
2025-01-31 13:49:53 -0800

    fix test

--- tests/app/job/test_rest.py
+++ tests/app/job/test_rest.py
@@ -517,10 +517,12 @@ def test_get_recent_notifications_for_job_in_reverse_order_of_job_number(
     assert resp["notifications"][0]["status"] == "virus-scan-failed"
     assert resp["notifications"][0]["job_row_number"] == 13
 
+    query_string = {"status": "delivered"}
     resp = admin_request.get(
-        "job.get_recent_notifications_for_service_job?status=delivered",
+        "job.get_recent_notifications_for_service_job",
         service_id=main_job.service_id,
         job_id=main_job.id,
+        **query_string,
     )
 
     print("RUNNING TEST TWO WITH LENGTH == 1")
