53852e110c772d315ad8aacef0c789a91324f10c
Kenneth Kehl <@kkehl@flexion.us>
2025-02-26 11:13:44 -0800

    temp

--- migrations/versions/0415_add_second_e2e_test_user.py
+++ migrations/versions/0415_add_second_e2e_test_user.py
@@ -5,9 +5,9 @@ import uuid
 import sqlalchemy as sa
 from alembic import op
 
-from app import db
-from app.dao.users_dao import get_user_by_email
-from app.models import User
+# from app import db
+# from app.dao.users_dao import get_user_by_email
+# from app.models import User
 from app.utils import utc_now
 
 revision = "0415_add_second_e2e_test_user"
@@ -17,8 +17,16 @@ down_revision = "0414_change_total_message_limit"
 def upgrade():
     email_address = os.getenv("NOTIFY_E2E_TEST_EMAIL_TWO")
     password = os.getenv("NOTIFY_E2E_TEST_PASSWORD_TWO")
+
+    # TODO remove
+    email_address = f"{uuid.uuid4()}@fake.gov"
+    password = f"{uuid.uuid4()}password"
+    # end TODO
+
     if not email_address or not password:
-        raise ValueError("Required variables [NOTIFY_E2E_TEST_EMAIL_TWO] and [NOTIFY_E2E_TEST_PASSWORD_TWO] missing!")
+        raise ValueError(
+            "Required variables [NOTIFY_E2E_TEST_EMAIL_TWO] and [NOTIFY_E2E_TEST_PASSWORD_TWO] missing!"
+        )
     name = f"e2e_test_user_{uuid.uuid4()}"
     data = {
         "id": uuid.uuid4(),
@@ -41,10 +49,15 @@ def upgrade():
     conn.execute(sa.text(insert_sql), data)
 
 
-def downgrade():
-    email_address = os.getenv("NOTIFY_E2E_TEST_EMAIL_TWO")
-    user_to_delete = get_user_by_email(email_address)
-    if not user_to_delete:
-        return
-    db.session.remove(user_to_delete)
-    db.session.commit()
+# def downgrade():
+#    email_address = os.getenv("NOTIFY_E2E_TEST_EMAIL_TWO")
+#    user_to_delete = get_user_by_email(email_address)
+#    if not user_to_delete:
+#        return
+#    db.session.remove(user_to_delete)
+#    db.session.commit()
+
+
+if __name__ == "__main__":
+    # Execute when the module is not initialized from an import statement.
+    upgrade()
