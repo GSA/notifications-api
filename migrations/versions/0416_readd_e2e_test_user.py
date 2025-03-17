19a510fa4503d4cc8d47948afb020fa0e332c1ea
Kenneth Kehl <@kkehl@flexion.us>
2025-03-17 15:31:38 -0700

    add auth type

--- migrations/versions/0416_readd_e2e_test_user.py
+++ migrations/versions/0416_readd_e2e_test_user.py
@@ -49,8 +49,8 @@ def upgrade():
         """
 
     insert_sql = """
-        insert into users (id, name, email_address, _password, mobile_number, state, created_at, password_changed_at, failed_login_count, platform_admin, email_access_validated_at)
-        values (:id, :name, :email_address, :password, :mobile_number, :state, :created_at, :password_changed_at, :failed_login_count, :platform_admin, :email_access_validated_at)
+        insert into users (id, name, email_address, _password, mobile_number, state, created_at, password_changed_at, failed_login_count, platform_admin, email_access_validated_at, auth_type)
+        values (:id, :name, :email_address, :password, :mobile_number, :state, :created_at, :password_changed_at, :failed_login_count, :platform_admin, :email_access_validated_at, :auth_type)
         """
     conn.execute(sa.text(delete_sql))
 
