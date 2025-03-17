74145502bd68f29c417e2954c9b04047dc68033a
Kenneth Kehl <@kkehl@flexion.us>
2025-03-17 15:15:55 -0700

    add comment

--- migrations/versions/0416_readd_e2e_test_user.py
+++ migrations/versions/0416_readd_e2e_test_user.py
@@ -41,6 +41,7 @@ def upgrade():
     }
     conn = op.get_bind()
 
+    # delete the old user
     delete_sql = """
         delete from users where email_address='e2e-test-notify-user@fake.gov'
         """
