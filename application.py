b7d96225fbbdb98f4b5ee8706cf9ec6677082a89
Kenneth Kehl <@kkehl@flexion.us>
2025-07-28 09:59:24 -0700

    move monkey patching

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 ##!/usr/bin/env python
 
-
 from __future__ import print_function
 
-from flask import Flask
-from werkzeug.serving import WSGIRequestHandler
+from gevent import monkey
 
-from app import create_app, socketio  # noqa: F401
+monkey.patch_all()  # this has to be called before other imports or monkey patching doesn't happen
+
+
+from flask import Flask  # noqa
+from werkzeug.serving import WSGIRequestHandler  # noqa
+
+from app import create_app, socketio  # noqa
 
 WSGIRequestHandler.version_string = lambda self: "SecureServer"
 
