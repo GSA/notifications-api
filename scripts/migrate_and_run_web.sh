4dde94433abe3766eba8ab7ec2feb130831d16b5
Kenneth Kehl <@kkehl@flexion.us>
2025-07-31 11:16:51 -0700

    put timeout back

--- scripts/migrate_and_run_web.sh
+++ scripts/migrate_and_run_web.sh
@@ -4,4 +4,4 @@ if [[ $CF_INSTANCE_INDEX -eq 0 ]]; then
   flask db upgrade
 fi
 
-exec newrelic-admin run-program gunicorn -c ${HOME}/gunicorn_config.py -b 0.0.0.0:6011 gunicorn_entry:application
+exec newrelic-admin run-program gunicorn -c ${HOME}/gunicorn_config.py gunicorn_entry:application
