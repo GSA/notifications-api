57ec2d6da340207cf2f52fee4278e6a64d18840c
Kenneth Kehl <@kkehl@flexion.us>
2025-09-11 13:10:02 -0700

    add another test

--- tests/app/service/test_rest.py
+++ tests/app/service/test_rest.py
@@ -3134,13 +3134,11 @@ def test_update_service_sms_sender(client, notify_db_session):
     assert not resp_json["is_default"]
 
 
-@pytest.mark.usefixtures("client", "notify_db_session")
+@pytest.mark.usefixtures("client")
 @settings(max_examples=10)
 @pytest.mark.parametrize("client", [True], indirect=True)
 @given(sms_sender=st.text(min_size=0, max_size=50), is_default=st.booleans())
-def test_fuzz_update_service_sms_sender(
-    client, notify_db_session, sms_sender, is_default
-):
+def test_fuzz_update_service_sms_sender(client, sms_sender, is_default):
     service = create_service()
     service_sms_sender = create_service_sms_sender(
         service=service, sms_sender="1235", is_default=False
