7a78dfe96fde8ba85292a68133fc51dab1dcfb55
Kenneth Kehl <@kkehl@flexion.us>
2025-10-03 07:40:12 -0700

    fix tests

--- tests/app/delivery/test_send_to_providers.py
+++ tests/app/delivery/test_send_to_providers.py
@@ -149,7 +149,7 @@ def test_should_send_personalised_template_to_correct_email_provider_and_persist
     mock_ses.send_email.return_value = "reference"
     mock_ses.name = "ses"
     mocker.patch(
-        "app.notification_provider_clients.get_client_by_name_and_type",
+        "app.delivery.send_to_providers.provider_to_use",
         return_value=mock_ses,
     )
 
