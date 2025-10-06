2336578fd35110e176b2cf29ba266a2e2d907c51
Kenneth Kehl <@kkehl@flexion.us>
2025-10-06 15:06:39 -0700

    lazy init encryption

--- app/__init__.py
+++ app/__init__.py
@@ -159,7 +159,9 @@ def get_encryption():
         fake_app = FakeEncryptionApp()
         sekret = "SEKRET_KEY"
         sekret = sekret.replace("KR", "CR")
-        fake_config = {"DANGEROUS_SALT": "SALTYSALTYSALTYSALTY", sekret: "FooFoo"}  # noqa
+        ds = os.environ.get("DANGEROUS_SALT")
+        sk = os.environ.get(sekret)
+        fake_config = {"DANGEROUS_SALT": ds, sekret: sk}  # noqa
         print(hilite(f"FAKE CONFIG is {fake_config}"))
         fake_app.init_fake_encryption_app(fake_config)
         encryption.init_app(fake_app)
