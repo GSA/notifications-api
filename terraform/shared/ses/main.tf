10810732939241e76d7033041f08728d75cfa6c9
Kenneth Kehl <@kkehl@flexion.us>
2025-10-24 09:17:24 -0700

    revert

--- terraform/shared/ses/main.tf
+++ terraform/shared/ses/main.tf
@@ -15,23 +15,7 @@ data "cloudfoundry_service" "ses" {
   name = "datagov-smtp"
 }
 
-# resource "cloudfoundry_service_instance" "ses_community" {
-#  name         = var.name
-#  space        = data.cloudfoundry_space.space.id
-#  service_plan = data.cloudfoundry_service.ses.service_plans["base"]
-#  json_params = jsonencode({
-#    region                        = var.aws_region
-#    domain                        = var.email_domain
-#    mail_from_subdomain           = var.mail_from_subdomain
-#    email_receipt_error           = var.email_receipt_error
-#    enable_feedback_notifications = true
-#  })
-#}
-
 resource "cloudfoundry_service_instance" "ses" {
-  providers = {
-    cloudfoundry = cloudfoundry.official
-  }
   name         = var.name
   space        = data.cloudfoundry_space.space.id
   service_plan = data.cloudfoundry_service.ses.service_plans["base"]
