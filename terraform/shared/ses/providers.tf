96a584a124fdbf9cf575295103c870e7acb5e4eb
Kenneth Kehl <@kkehl@flexion.us>
2025-10-24 09:24:08 -0700

    revert

--- terraform/shared/ses/providers.tf
+++ terraform/shared/ses/providers.tf
@@ -1,10 +1,7 @@
 terraform {
   required_version = "~> 1.7"
   required_providers {
-    cloudfoundry = {
-      source  = "cloudfoundry/cloudfoundry"
-      version = "1.9.0"
-    }
+
     cfcommunity = {
       source  = "cloudfoundry-community/cloudfoundry"
       version = "0.53.1"
@@ -12,13 +9,6 @@ terraform {
   }
 }
 
-# Official provider (should be default but aliased for now)
-provider "cloudfoundry" {
-  alias    = "official"
-  api_url  = "https://api.fr.cloud.gov"
-  user     = var.cf_user
-  password = var.cf_password
-}
 
 # Community provider (should be aliased but default for now)
 provider "cfcommunity" {
